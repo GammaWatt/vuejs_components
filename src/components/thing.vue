ba428acf4bd530794f9990aab62ebef9b306081e
GammaWatt <GammaWatt@users.noreply.github.com>
2021-05-25 12:18:25 -0500

    Background set, but attribution not working.

---
+++ src/components/thing.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="div-with-background" :style="style">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "image"
+  ]
+  , data () {
+    return {
+      style: {
+        backgroundImage: `url(${this.image})`
+        , backgroundPosition: 'center'
+        , backgroundSize: 'cover'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.div-with-background {
+  height: 100px;
+  min-width: content;
+  min-height: content;
+}
+
+.div-with-background > div {
+  color: white;
+  position: absolute;
+  display: block;
+  margin: 0 auto;
+  bottom: 0;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div>
+    <image-vue :image="img1_url">
+      <div>Attribution</div>
+    </image-vue>
+  </div>  
+</template>
+
+<script>
+import Image from '@/components/thing.vue'
+
+export default {
+  data () {
+    return {
+      'img1_url': 'https://drive.google.com/uc?export=view&id=1yIiZKiMcLKSuVBEzcE0od2eCIzI169cy'
+    }
+  }
+  , components: {
+    'image-vue': Image
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
